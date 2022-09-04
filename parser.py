86365bbf5d30dfa840e00ed5152f6cfda3decd82
cidTomsk <61188943+cidTomsk@users.noreply.github.com>
2022-09-04 10:37:05 +0700

    pain

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
-import json
+import sys
+import numpy as np
 import time
 import requests
 import asyncio
-import aiohttp   # https://github.com/pythontoday/scrap_tutorial/blob/master/lesson9/main_asyncio.py
+import aiohttp
 from bs4 import BeautifulSoup
 
 
@@ -24,52 +25,76 @@ class Parser:
         """
         response = requests.get(url=self.url, headers=self.headers)
         soup = BeautifulSoup(response.text, "html.parser")
-        print(f"Log  {soup}")
         all_news = soup.find_all('tr', class_='athing')
         for news in all_news:
             # https://news.ycombinator.com/item?id=32624461  шаблон страницы с комментом
-            print(f"Log  news \n {news} \n id {news['id']}")
-            print(f"Log  news2 \n {news.contents[4].a['href']}  {type(news.contents[4])} \n")
-            news_title = news.text
+            news_title = news.text.replace("\n", "")
             url = news.contents[4].a['href']
+            if "http" not in url:
+                continue
             self.news[news_title] = {"news_url": url, "id": news['id'],
                                      "comment_url": f"https://news.ycombinator.com/item?id={news['id']}"}
-            print(self.news.keys())
 
-    async def download_news(self, session, news_title):
+    async def get_data(self):
+        async with aiohttp.ClientSession() as session:
+            self.find_news()
+            tasks = []
+            for news_title in list(self.news.keys())[:2]:  # TODO full list
+                task = asyncio.create_task(self.download_news(news_title=news_title, session=session))
+                tasks.append(task)
+            await asyncio.gather(*tasks)
+
+    async def download_news(self, session, news_title, retries=3):
         """
         Скачивание новости: контент новости, страница комментариев, ссылки в комментариях
         """
         news = self.news[news_title]
-        async with session.get(url=news["news_url"], headers=self.headers) as news_page_response:
-            # news_page_response = requests.get(url=news["news_url"], headers=self.headers)
-            news["content"] = news_page_response.text
-            news_comment_response = requests.get(url=news["comment_url"], headers=self.headers)
-            news["comment_page_content"] = news_comment_response.text
-            soup = BeautifulSoup(news_comment_response.text, "html.parser")
-            all_tags_a_from_comments = soup.find_all('a')
-            all_links_from_comments = []
-            for tag in all_tags_a_from_comments:
-                href = tag["href"]
-                if "http" in href:
-                    all_links_from_comments.append(href)
-            print(f"Log all_links_from_comments {all_links_from_comments}")
-            news_comment_links_data = []
-            for comment_link in all_links_from_comments:
-                comment_link_response = requests.get(url=comment_link, headers=self.headers)
-                news_comment_links_data.append({f"{comment_link}": comment_link_response.text})
-            news["news_comment_links_data"] = news_comment_links_data
-            self.news[news_title] = news
+        while retries:
+            try:
+                async with session.get(url=news["news_url"], headers=self.headers) as news_page_response:
+                    news["content"] = await news_page_response.text()
+                    async with session.get(url=news["comment_url"], headers=self.headers) as news_comment_response:
+                        news["comment_page_content"] = await news_comment_response.text()
+                        soup = BeautifulSoup(news_comment_response.text, "html.parser")
+                        all_tags_a_from_comments = soup.find_all('a')
+                        all_links_from_comments = []
+                        for tag in all_tags_a_from_comments:
+                            href = tag["href"]
+                            if "http" in href:
+                                all_links_from_comments.append(href)
+                        news_comment_links_data = []
+                        for comment_link in all_links_from_comments:
+                            if "http" in comment_link:
+                                comment_link_response = requests.get(url=comment_link, headers=self.headers)
+                            else:
+                                comment_link_response = requests.get(url=f"https://news.ycombinator.com/{comment_link}",
+                                                                     headers=self.headers)
+                            news_comment_links_data.append({f"{comment_link}": comment_link_response.text})
+                        news["news_comment_links_data"] = news_comment_links_data
+                        self.news[news_title] = news
+
+            except Exception as exc:   #aiohttp.client_exceptions.ClientConnectorError
+                retries -= 1
+            else:
+                break
 
     def save_to_file(self):
-        json.dump(self.news, open("dump.json", 'w'))
+        dictionary = {}
+        for key in list(self.news.keys()):
+            dictionary[key] = self.news[key]  # иначе TypeError: can't pickle multidict._multidict.CIMultiDictProxy objects
+        np.save('my_file.npy', dictionary)
+
+    def load_dict_from_file(self):
+        return np.load('my_file.npy', allow_pickle='TRUE').item()
+
 
 
 def main():
     parser = Parser(url="https://news.ycombinator.com/")
-    parser.find_news()
-    parser.download_news(news_title=list(parser.news.keys())[0])
+    asyncio.run(parser.get_data())
     parser.save_to_file()
+    read_dictionary = parser.load_dict_from_file()
+    print(f" SIZE {sys.getsizeof(read_dictionary)}")
     finish_time = time.time() - start_time
     print(f"Затраченное на работу скрипта время: {finish_time}")
 
